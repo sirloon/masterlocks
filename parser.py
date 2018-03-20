d3ac206d1cd261f91dd9ba42bf295436acffb10c
Sebastien Lelong <slelong@scripps.edu>
2018-03-20 12:10:00 -0700

    initial parser/manifest

---
+++ parser.py
@@ -0,0 +1,19 @@
+import csv, os
+from pprint import pprint
+
+def load_data(data_folder):
+    pinsfile = os.path.join(data_folder,"pins.csv")
+    assert os.path.exists(pinsfile)
+    r = csv.reader(open(pinsfile,"r").readlines(),delimiter='\t')
+    for l in r:
+        d = {"lock_num":l[0],"pins_num":l[1]}
+        if len(l) == 3:
+            d["has_security_pins"] = (l[2] =="yes" and True) or \
+                                     (l[2] == "no" and False) or None
+        yield d
+
+
+if __name__ == "__main__":
+    g = load_data(".")
+    for d in g:
+        pprint(d)
