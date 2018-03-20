0ef56b428dd998bb70aac56351a45c9521740453
Sebastien Lelong <slelong@scripps.edu>
2018-03-20 13:02:37 -0700

    fix manifest+parser

--- parser.py
+++ parser.py
@@ -4,9 +4,9 @@ from pprint import pprint
 def load_data(data_folder):
     pinsfile = os.path.join(data_folder,"pins.csv")
     assert os.path.exists(pinsfile)
-    r = csv.reader(open(pinsfile,"r").readlines(),delimiter='\t')
+    r = csv.reader(open(pinsfile,"r").readlines()[1:],delimiter='\t')
     for l in r:
-        d = {"lock_num":l[0],"pins_num":l[1]}
+        d = {"_id":l[0],"pins_num":l[1]}
         if len(l) == 3:
             d["has_security_pins"] = (l[2] =="yes" and True) or \
                                      (l[2] == "no" and False) or None
